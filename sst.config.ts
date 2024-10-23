929ed33d59ea7becd6b25b7eb2e595c4e46f7471
Colin <cwrmcolin@gmail.com>
2024-10-22 23:41:00 -0700

    dev: basic repo set-up for nextjs over aws lambdas, s3, and cloudfront

---
+++ sst.config.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+/* eslint-disable */
+/// <reference path="./.sst/platform/config.d.ts" />
+
+export default $config({
+  app(input) {
+    return {
+      name: "aws-nextjs",
+      removal: input?.stage === "production" ? "retain" : "remove",
+      home: "aws",
+    };
+  },
+  async run() {
+    new sst.aws.Nextjs(
+      "MyWeb");
+  },
+});
