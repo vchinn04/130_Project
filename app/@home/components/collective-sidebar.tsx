6a2c5cf2331e1cfdc899bf92a830a9f96617dd3a
Colin <cwrmcolin@gmail.com>
2024-11-17 12:33:29 -0800

    fix: the MemberSidebar component should be used as a self-closing tag only becasue it does not take any children

--- app/@home/components/collective-sidebar.tsx
+++ app/@home/components/collective-sidebar.tsx
@@ -83,7 +83,7 @@ export default function CollectiveSidebar({
         </SidebarFooter>
       </Sidebar>
 
-      <MembersSidebar groups={groups}> </MembersSidebar>
+      <MembersSidebar groups={groups}/>
     </>
   );
 }
