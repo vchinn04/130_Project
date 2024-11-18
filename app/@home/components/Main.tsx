f6d7f3eeae8a71c0581ee26ec7c3b85d3239c59a
Victor Chinnappan <vchinn04@gmail.com>
2024-11-18 00:27:17 -0800

    Added chat UI components

--- app/@home/components/Main.tsx
+++ app/@home/components/Main.tsx
@@ -1,16 +1,21 @@
 "use client";
-import React from 'react'
-import { useState } from 'react';
-import PopUp from './PopUp';
-import { Button } from "@/components/ui/button"
+import React from "react";
+import { useState } from "react";
+import PopUp from "./PopUp";
+import { Button } from "@/components/ui/button";
 const Main = () => {
-    const [isPopupOpen, setIsPopupOpen] = useState(false);
-return (
-    <main className='grow relative' >
-        <Button className='absolute bottom-0 right-0 mr-2 mb-2' onClick={() => setIsPopupOpen(!isPopupOpen)}>Toggle Popup</Button>
-        {isPopupOpen && <PopUp setIsPopupOpen={setIsPopupOpen} />}
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+  return (
+    <main className="relative">
+      <Button
+        className="absolute bottom-0 right-5 mr-2 mb-2"
+        onClick={() => setIsPopupOpen(!isPopupOpen)}
+      >
+        Toggle Popup
+      </Button>
+      {isPopupOpen && <PopUp setIsPopupOpen={setIsPopupOpen} />}
     </main>
-)
-}
+  );
+};
 
-export default Main
+export default Main;
