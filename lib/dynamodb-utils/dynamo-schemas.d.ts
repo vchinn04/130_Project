12fece861347f6c3b121b8499008e0e42e824374
Colin <cwrmcolin@gmail.com>
2024-11-28 12:22:27 -0800

    dev: added types for the schemas to define the immutible fields in the relevant tables. This is intended to make the api for the database table updating functions more flexible and maintainable

--- lib/dynamodb-utils/dynamo-schemas.d.ts
+++ lib/dynamodb-utils/dynamo-schemas.d.ts
@@ -1,35 +1,54 @@
-export type UserId = string;  // from cognito
+export type UserId = string;  // sourced from cognito
 export type GroupId = string; // UUID v4, generated at creation time
 
 export type Team = {
   members: UserId[];          // simple list of the members in the team
 };
 
-export type TeamSubtable = {
+export interface TeamSubtable {
   groupId: GroupId;            // dynamodb hash key (primary index)
   subTable: "teams";           // dynamodb sort key (secondary index)
   generatedAt: Date;           // the date and time the teams were last generated
   teams: Team[];               // list of the teams in the group (team numbers derived from index in array)
 };
 
-export type GroupInfoSubtable = {
+// properties that are immutable for TeamSubtable entries after they are created
+export type ImmutableTeamSubtableProperties = Omit<
+  TeamSubtable,
+  | typeof TeamSubtable.prototype.groupId
+  | typeof TeamSubtable.prototype.subTable
+>;
+
+// ------------------------------------------------------------------------------------------------
+
+export interface GroupInfoSubtable {
   groupId: GroupId;           // dynamodb hash key (primary index)
   subTable: "info";           // dynamodb sort key (secondary index)
+  createdAt: Date;            // the date and time the group was created
   displayName: string;        // optionally-settable name of the group
   owner: UserId;              // the user id of the group owner, who can make changes and manage the group
   locked: boolean;            // whether new people can join a group.
   prompt: string;             // the prompt that the group owner sets for new members to join the group
   memberCount: number;        // the number of members in the group
   teamCount: number;          // the number of teams in the group
-  createdAt: Date;            // the date and time the group was created
 };
+
+// properties that are immutable for GroupInfoSubtable entries after they are created
+export type ImmutableGroupInfoProperties = Omit<
+  GroupInfoSubtable,
+  | typeof GroupInfoSubtable.prototype.groupId
+  | typeof GroupInfoSubtable.prototype.subTable
+  | typeof GroupInfoSubtable.prototype.createdAt
+>;
+
+// ------------------------------------------------------------------------------------------------
 
 export type Member = {
   ready: boolean;               // indicates if the user is ready to be matched to a team
   promptAnswer: string;         // link to an s3 object where the user's prompt answer is stored
 };
 
-export type GroupMembersSubtable = {
+export interface GroupMembersSubtable {
   groupId: GroupId;                 // dynamodb hash key (primary index)
   subTable: "members";              // dynamodb sort key (secondary index)
   members: Record<UserId, Member>;  // json object of the members in the group
