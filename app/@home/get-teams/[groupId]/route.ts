611d248f60a182efc2eb9230515b34b68d357b8d
Colin <cwrmcolin@gmail.com>
2024-12-01 13:29:01 -0800

    feat: added api endpoint for getting the teams subtable entries for a group

---
+++ app/@home/get-teams/[groupId]/route.ts
@@ -0,0 +1,16 @@
+import { getTeams } from "@/lib/db-utils/dynamo-queries";
+
+export async function GET(
+  request: Request,
+  { params }: { params: Promise<{ groupId: string }> }
+): Promise<Response> {
+  const groupId = (await params).groupId;
+
+  try {
+    const group = await getTeams(groupId);
+    return Response.json(group, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return Response.json({ error: "Failed to get user group in request handler" }, { status: 500 }); // (should redirect to failure page)
+  }
+}
