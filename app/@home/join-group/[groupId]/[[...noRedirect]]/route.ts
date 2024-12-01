2de2f08943705c692a01caec09c73b29e41407f1
Colin <cwrmcolin@gmail.com>
2024-12-01 13:24:00 -0800

    feat: added api endpoint for joining groups

--- app/@home/join-group/[groupId]/[[...noRedirect]]/route.ts
+++ app/@home/join-group/[groupId]/[[...noRedirect]]/route.ts
@@ -3,8 +3,11 @@ import { addOrUpdateGroupMember } from "@/lib/db-utils/dynamo-queries";
 import { auth } from "@clerk/nextjs/server";
 import { UserId } from "@/types/globals";
 
-export async function GET(request: Request, { params }: { params: Promise<{ groupId: string, noRedirect: string[] }> }) {
-  const { groupId, noRedirect } = await params;
+export async function GET(
+  request: Request,
+  { params }: { params: Promise<{ groupId: string }> }
+): Promise<Response> {
+  const groupId = (await params).groupId;
 
   try {
     // if the groupId is already in the user's joined or owned groups, just return to the home page
@@ -17,13 +20,21 @@ export async function GET(request: Request, { params }: { params: Promise<{ grou
     return Response.json({ error: "Failed to get user group metadata in request handler" }, { status: 500 }); // (should redirect to failure page)
   }
 
+  //TODO: check if the group is locked
+
   // otherwise, try to add the user to the group in the database
   const { sessionClaims } = await auth();
   try {
-    await addOrUpdateGroupMember(groupId, sessionClaims?.userId as UserId, {
-      ready: false,
+    try {
+      await addOrUpdateGroupMember(groupId, sessionClaims?.userId as UserId, {
+        ready: false,
       promptAnswer: "",
-    });
+      });
+    } catch (error) {
+      console.error(error);
+      return Response.json({ error: "Failed to add user to group in database" }, { status: 500 }); // should redirect to failure page
+    }
+
     await clerkAddJoinedGroup(groupId);
     return Response.redirect(new URL("/", request.url), 307);
 
