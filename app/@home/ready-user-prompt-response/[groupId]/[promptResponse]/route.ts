1fdad1dbdadec6442850747ab9aaa560b3e1daea
Colin <cwrmcolin@gmail.com>
2024-12-02 14:16:32 -0800

    dev: added the api endpoints for toggling the user prompt off and on

--- app/@home/ready-user-prompt-response/[groupId]/[promptResponse]/route.ts
+++ app/@home/ready-user-prompt-response/[groupId]/[promptResponse]/route.ts
@@ -1,18 +1,24 @@
+import { uploadPromptAnswer } from "@/lib/db-utils/bucket-queries";
 import { addOrUpdateGroupMember } from "@/lib/db-utils/dynamo-queries";
 import { UserId } from "@/types/globals";
 import { auth } from "@clerk/nextjs/server";
 
 export async function GET(
   req: Request,
-  { params }: { params: Promise<{ groupId: string }> }
+  { params }: { params: Promise<{ groupId: string, promptResponse: string }> }
 ) {
   try {
     // get the auth session token of the requester
     const authRes = await auth();
+    // const { promptResponse } = await req.json();
 
-    // update the user's ready status to false
+    // upload the prompt response to the bucket if one was provided.
+    if ((await params).promptResponse) {
+      await uploadPromptAnswer((await params).groupId, authRes?.userId as UserId, (await params).promptResponse);
+    }
+
     await addOrUpdateGroupMember((await params).groupId, authRes?.userId as UserId, {
-      ready: false,
+      ready: true,
       promptAnswer: "",
     });
 
