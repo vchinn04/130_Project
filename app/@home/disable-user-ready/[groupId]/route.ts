54ef204412f4645897a240573d59d367b00c6fe2
Colin <cwrmcolin@gmail.com>
2024-12-01 17:27:18 -0800

    dev: added more endpoints for the api to satisfy frontend needs

---
+++ app/@home/disable-user-ready/[groupId]/route.ts
@@ -0,0 +1,24 @@
+import { addOrUpdateGroupMember } from "@/lib/db-utils/dynamo-queries";
+import { UserId } from "@/types/globals";
+import { auth } from "@clerk/nextjs/server";
+
+export async function POST(
+  req: Request,
+  { params }: { params: Promise<{ groupId: string }> }
+) {
+  try {
+    // get the auth session token of the requester
+    const authRes = await auth();
+
+    // update the user's ready status to false
+    await addOrUpdateGroupMember((await params).groupId, authRes?.userId as UserId, {
+      ready: false,
+      promptAnswer: "",
+    });
+
+    return Response.json({ success: true }, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return Response.json({ error: "Failed to set user prompt response" }, { status: 500 });
+  }
+}
